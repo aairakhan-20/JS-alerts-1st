18f896260389d6e1375ab2e87a2321d52f3f60cc
Aaira Khan <aairasalmankhan2008@gmail.com>
2024-12-03 13:33:45 +0500

    task done

---
+++ app.js
@@ -0,0 +1,4 @@
+alert("Error! Please enter a valid password");
+alert("Welcome To JS World...!!! \n ...Happy Coding...");
+alert("Hey There...!!! \n I can run through my web browser console...");
+document.write ("Welcome To 1st Assignment of JS")
